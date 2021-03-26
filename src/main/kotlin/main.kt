f31963b73cfb2d011992e7aaa7fcffeed2623c74
hhs823 <42017905+hhs823@users.noreply.github.com>
2021-03-26 11:25:28 +0900

    update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,52 @@
+import java.lang.StringBuilder
+import kotlin.math.min
+
 class Solution {
     fun solution(s: String): Int {
-        var answer = 0
+        var answer = s.length
+
+        for(i in 1..s.length / 2) {
+            answer = min(answer, compress(s, i))
+        }
+
         return answer
+    }
+
+    private fun compress(s: String, size: Int): Int {
+        val result = StringBuilder()
+
+        var split = ArrayList<String>()
+
+        var word = StringBuilder()
+        s.split("").filter { it.isNotEmpty() }.forEach {
+            word.append(it)
+            if (word.length == size) {
+                split.add(word.toString())
+                word = StringBuilder()
+            }
+        }
+        if(word.isNotEmpty())
+            split.add(word.toString())
+
+        var cnt = 1
+        var prev = split[0]
+
+        for (i in 1 until split.size) {
+            if (split[i] == prev) cnt++
+            else {
+                if(cnt != 1)
+                    result.append(cnt).append(prev)
+                else
+                    result.append(prev)
+                prev = split[i]
+                cnt = 1
+            }
+        }
+        if(cnt != 1)
+            result.append(cnt).append(prev)
+        else
+            result.append(prev)
+
+        return result.toString().length
     }
 }
