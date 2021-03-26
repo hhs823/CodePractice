8d08f3140356ab41742e6f4e1560789cbe9e1586
hhs823 <42017905+hhs823@users.noreply.github.com>
2021-03-26 09:25:48 +0900

    update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,8 @@
-import java.util.*
-
 class Solution {
-    fun solution(board: Array<IntArray>, moves: IntArray): Int {
-        var answer = 0
+    fun solution(n: Int): IntArray {
+        var answer: IntArray = intArrayOf()
 
-        val init = Array(board.size) { Stack<Int>() }
-        for (i in init.size - 1 downTo 0) {
-            board[i].forEachIndexed { index, i -> if (i != 0) init[index].push(i) }
-        }
 
-        val stack = Stack<Int>()
-        moves.forEach loop@{
-            if (init[it - 1].isEmpty()) return@loop
-            val pick = init[it - 1].pop()
-            if (stack.isNotEmpty() && stack.peek() == pick) {
-                stack.pop()
-                answer += 2
-            } else {
-                stack.push(pick)
-            }
-        }
 
         return answer
     }
